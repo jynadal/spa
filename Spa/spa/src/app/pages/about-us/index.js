0015cab3203fd7682e763f5500fe20cfec39da8b
jynadal <nadaljeanyves@hotmail.fr>
2020-11-24 10:06:43 +0100

    Dynamique Seo of title and description

--- Spa/spa/src/app/pages/about-us/index.js
+++ Spa/spa/src/app/pages/about-us/index.js
@@ -4,7 +4,7 @@ import Seo from '../../seo'
 export default function AboutUs() {
     return (
         <div>
-            <Seo title="Welcome the about us page" description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis,
+            <Seo title="Welcome the about us page" description="About us ipsum dolor sit amet consectetur adipisicing elit. Corporis,
              architecto explicabo in beatae numquam aspernatur, quos velit consequuntur" />
             <h1>About Us</h1>
             
--- Spa/spa/src/app/seo/index.jsx
+++ Spa/spa/src/app/seo/index.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 export default function Seo({title, description}) {
     document.querySelector('title').innerHTML = title;
-    //console.dir(document.querySelector('meta[2]').innerHTML);
+    //console.dir(document.querySelector('meta[Name="description"]').setAttribute('content', description));
+    document.querySelector('meta[Name="description"]').setAttribute('content', description)
 
     return null;
 }
