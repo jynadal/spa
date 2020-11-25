e029fd1949f99055260ea8835350c2a3547aabc8
jynadal <nadaljeanyves@hotmail.fr>
2020-11-25 21:58:42 +0100

    spa cart 1 end

--- Spa/spa/src/app/pages/products/index.js
+++ Spa/spa/src/app/pages/products/index.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import Seo from '../../seo'
+import './styles.css'
+
+import products from "../../data/products.json"
+import Product from "./Product"
 
 export default function ProductsPage() {
     return (
@@ -7,6 +11,11 @@ export default function ProductsPage() {
             <Seo title="Here the products page" description="All our shit that we sell to you us ipsum dolor sit amet consectetur adipisicing elit. Corporis,
              architecto explicabo in beatae numquam aspernatur, quos velit consequuntur" />
             <h1>Products we sell to you!</h1>
+            <div className="products">
+                {products.map(product => (
+                    <Product {...product} />
+                ))}
+            </div>
             
         </div>
     )
